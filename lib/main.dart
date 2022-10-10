2d460da3a21134be56c05e9c2994c35238e90429
Dong Quan <dongquan1212@gmail.com>
2022-10-10 15:11:04 +0700

    play with statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ void main() => runApp(MaterialApp(
       home: MyCard(),
     ));
 
-class MyCard extends StatelessWidget {
+class MyCard extends StatefulWidget {
   const MyCard({super.key});
+
+  @override
+  State<MyCard> createState() => _MyCardState();
+}
+
+class _MyCardState extends State<MyCard> {
+  int currentLevel = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +23,13 @@ class MyCard extends StatelessWidget {
           title: Text("Quan Information"),
           backgroundColor: Colors.grey[850],
         ),
+        floatingActionButton: ElevatedButton(
+            onPressed: () {
+              setState(() {
+                currentLevel += 1;
+              });
+            },
+            child: Icon(Icons.add)),
         body: Padding(
           padding: EdgeInsets.all(20.0),
           child: Column(
@@ -76,6 +90,25 @@ class MyCard extends StatelessWidget {
                   ),
                   Text(
                     "21",
+                    style: TextStyle(
+                        color: Colors.amber,
+                        letterSpacing: 2.0,
+                        fontSize: 20.0),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Current Flutter Skill Level: ",
+                    style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+                  ),
+                  Text(
+                    '$currentLevel',
                     style: TextStyle(
                         color: Colors.amber,
                         letterSpacing: 2.0,
